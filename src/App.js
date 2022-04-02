eb731c168c46086d2c9f2e8cf1efac0dd1179f4f
cjohnson74 <cjohnson74.tech@gmail.com>
2022-04-02 10:36:40 -0400

    added algorithms array

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
 import logo from "./logo.svg";
 import "./App.css";
 import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
+import { Fragment } from 'react';
+import { Popover, Transition } from '@headlessui/react';
+
+const algorithms = [
+  {
+    name: "Dijkstra's Algorithm",
+    description: "The father of pathfinding algorithms; guarantees the shortest path",
+    href: "#",
+  },
+  {
+    name: "A* Search (coming soon)",
+    description: "arguably the best pathfinding algorithm; uses heuristics to guarantee the shortest path much faster than Dijkstra's Algorithm",
+    href: "#",
+  },
+  {
+    name: "Greedy Best-first Search (coming soon)",
+    description: "",
+    href: "#",
+  },
+  {
+    name: "Swarm Algorithm (coming soon)",
+    description: "",
+    href: "#",
+  },
+  {
+    name: "Convergent Swarm Algorithm (coming soon)",
+    description: "",
+    href: "#",
+  },
+  {
+    name: "Bidirectional Swarm Algorithm (coming soon)",
+    description: "",
+    href: "#",
+  },
+  {
+    name: "Breadth-first Search (coming soon)",
+    description: "",
+    href: "#",
+  },
+  {
+    name: "Depth-first Search (coming soon)",
+    description: "",
+    href: "#",
+  }
+]
 
 function App() {
   return (
     <div className="App">
+
       <PathfindingVisualizer></PathfindingVisualizer>
     </div>
   );
