2c0f0ad3e269929b92d8e7d4a5e7ac626144e07f
cjohnson74 <cjohnson74.tech@gmail.com>
2022-04-03 13:19:56 -0400

    pushing to heroku

--- src/App.js
+++ src/App.js
@@ -1,10 +1,44 @@
 import logo from "./logo.svg";
-import "./App.css";
 import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
+import "../src/index.css";
+import "../src/App.css"
+import "./scss/Custom.scss";
+import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">AlgoViz</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#features">Features</Nav.Link>
+              <Nav.Link href="#pricing">Pricing</Nav.Link>
+              <NavDropdown title="Algorithms" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Dank memes
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
       <PathfindingVisualizer></PathfindingVisualizer>
     </div>
   );
